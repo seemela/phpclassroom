afd26f04c2971c5e79925d21a2fbb661f58a5416
seemela <73819173+seemela@users.noreply.github.com>
2021-11-26 20:41:44 +0700

    Update covid.php

--- covid.php
+++ covid.php
@@ -1,15 +1,19 @@
 <?php
-  $json = file_get_contents('https://covid19.ddc.moph.go.th/api/Cases/today-cases-all');
+  $json = file_get_contents("https://covid19.ddc.moph.go.th/api/Cases/today-cases-all");
   $data = json_decode($json);
 
   echo "<table border='1'>";
-    for($i=0;$i<100;$i++){
-      echo "<tr>";
-      echo "<td>คอลัมน์<td>";
-  foreach ( $data[0] as $key => $val ){
-    echo $key.":".$val."<br>";
-      echo "<td>คอลัมน์</td>";
+
+ foreach ( $data[0] as $key => $val ){
+    echo "<tr>";
+    echo "<td>";
+    echo $val."<br>"; 
+    echo "</td>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "</tr>";
   }
-  echo "</tr>";
+
   echo "</table>";
 ?>
